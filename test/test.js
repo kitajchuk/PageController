aff193ae54024e586d20df61cf258c8875c356d1
kitajchuk <kitajchuk@gmail.com>
2017-03-14 15:24:03 -0700

    use Router v0.4. add webpack 2 and test.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,21 @@
 var PageController = require( "../PageController" );
 var controller = new PageController();
 
-
-console.log( controller );
+controller.setConfig( ["*"] );
+controller.setModules( [] );
+controller.on( "page-controller-router-samepage", function () {
+    console.log( "samepage" );
+});
+controller.on( "page-controller-router-transition-out", function () {
+    console.log( "out" );
+});
+controller.on( "page-controller-router-refresh-document", function () {
+    console.log( "refresh" );
+});
+controller.on( "page-controller-router-transition-in", function () {
+    console.log( "in" );
+});
+controller.on( "page-controller-initialized-page", function () {
+    console.log( "init" );
+});
+controller.initPage();
